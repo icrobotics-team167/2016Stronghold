cfc03f4b05c622ed9bf8c87d448db23bf1fb4422
Kaz Smith <icroboticsteam167@googlegroups.com>
2016-02-22 18:29:49 -0600

    Remove software limit switches on def arm

--- src/org/iowacityrobotics/y2016/stronghold/DefenseArm.java
+++ src/org/iowacityrobotics/y2016/stronghold/DefenseArm.java
@@ -3,20 +3,16 @@ package org.iowacityrobotics.y2016.stronghold;
 import org.iowacityrobotics.lib167.component.IComponent;
 import org.iowacityrobotics.lib167.util.MathUtils;
 
-import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Talon;
 
 public class DefenseArm implements IComponent<Double> {
 	
 	private Talon armTalon;
-	private DigitalInput top, bot;
 	private final int port;
 	private double val;
 	
 	public DefenseArm(int s, int topLim, int botLim) {
 		armTalon = new Talon(s);
-		top = new DigitalInput(topLim);
-		bot = new DigitalInput(botLim);
 		this.port = s;
 	}
 
@@ -28,7 +24,7 @@ public class DefenseArm implements IComponent<Double> {
 
 	@Override
 	public void setState(Double value) throws UnsupportedOperationException {
-		val = MathUtils.clamp(value, top.get() ? -1D : 0D, bot.get() ? 1D : 0D);
+		val = MathUtils.clamp(value, -1D, 1D);
 		armTalon.set(val);
 	}
 
