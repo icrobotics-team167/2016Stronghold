bb9fc0031fa64a3c901d0aa7750a16e341cfb5cf
phantamanta44 <evandalong@gmail.com>
2018-08-19 23:12:22 -0500

    Switch to xbox drive scheme for jr bots demo

--- src/main/java/org/iowacityrobotics/y2016/Nums.java
+++ src/main/java/org/iowacityrobotics/y2016/Nums.java
@@ -2,11 +2,14 @@ package org.iowacityrobotics.y2016;
 
 public class Nums {
 
-    public static int CTRL_L = 3;
-    public static int CTRL_R = 0;
+    public static int CTRL = 1;
 
-    public static int AXIS_X = 0;
-    public static int AXIS_Y = 1;
-    public static final int THROTTLE = 2;
+    public static int AXIS_Y1 = 1;
+    public static int AXIS_Y2 = 5;
+    public static final int BELT_CTRL = 2;
+    public static final int FIRE_LEVER = 3;
+
+    public static final int REVERSE = 5;
+    public static final int TRIGGER = 6;
 
 }
--- src/main/java/org/iowacityrobotics/y2016/Robot.java
+++ src/main/java/org/iowacityrobotics/y2016/Robot.java
@@ -20,16 +20,18 @@ public class Robot implements IRobotProgram {
 
     @Override
     public void init() {
-        Source<Double> joyLeft = SourceSystems.CONTROL.axis(Nums.CTRL_L, Nums.AXIS_Y)
-                .map(MapperSystems.CONTROL.deadZoneD(0.1D));
-        Source<Double> joyRight = SourceSystems.CONTROL.axis(Nums.CTRL_R, Nums.AXIS_Y)
-                .map(MapperSystems.CONTROL.deadZoneD(0.1D));
-        Source<Double> throttle = SourceSystems.CONTROL.axis(Nums.CTRL_R, 3)
-                .map(Data.mapper(d -> (1 - d) / 2D));
-        Source<Boolean> reverse = SourceSystems.CONTROL.button(Nums.CTRL_R, 2)
+        Source<Double> joyLeft = SourceSystems.CONTROL.axis(Nums.CTRL, Nums.AXIS_Y1)
+                .map(MapperSystems.CONTROL.deadZoneD(0.1D))
+                .map(Data.mapper(d -> d * 0.8D));
+        Source<Double> joyRight = SourceSystems.CONTROL.axis(Nums.CTRL, Nums.AXIS_Y2)
+                .map(MapperSystems.CONTROL.deadZoneD(0.1D))
+                .map(Data.mapper(d -> d * 0.8D));
+//        Source<Double> throttle = SourceSystems.CONTROL.axis(Nums.CTRL, Nums.THROTTLE)
+//                .map(Data.mapper(d -> (1 - d) / 2D));
+        Source<Boolean> reverse = SourceSystems.CONTROL.button(Nums.CTRL, Nums.REVERSE)
                 .map(MapperSystems.CONTROL.toggle());
-        Source<Vector2> srcDrive = joyLeft.inter(joyRight, Data.inter((l, r) -> new Vector2(l, r)))
-                .inter(throttle, Data.inter((v, t) -> v.multiply(-t)))
+        Source<Vector2> srcDrive = joyLeft.inter(joyRight, Data.inter(Vector2::new))
+                .inter(/*throttle*/Data.source(() -> 1D), Data.inter((v, t) -> v.multiply(-t)))
                 .inter(reverse, Data.inter((v, r) -> {
                     if (r) {
                         double x = -v.y();
@@ -40,22 +42,30 @@ public class Robot implements IRobotProgram {
         MotorTuple4 motors = MotorTuple4.ofTalons(1, 8, 2, 9);
         Sink<Vector2> drive = SinkSystems.DRIVE.dualTread(motors);
 
-        Source<Double> ballBeltCtrl = SourceSystems.CONTROL.button(Nums.CTRL_L, 2)
+        Source<Double> ballBeltCtrl = SourceSystems.CONTROL.axis(Nums.CTRL, Nums.BELT_CTRL)
+                .map(MapperSystems.CONTROL.booleanify(0.5D))
                 .map(new BallBeltController());
-        Source<Double> triggerPrefire = SourceSystems.CONTROL.axis(Nums.CTRL_L, Nums.THROTTLE)
-                .map(Data.mapper(d -> (1 - d) / 4D + 0.5D))
-                .inter(SourceSystems.CONTROL.button(Nums.CTRL_L, 1), Data.inter((v, t) -> t ? v : 0));
-        Source<Double> triggerFire = SourceSystems.CONTROL.button(Nums.CTRL_R, 1)
+        Source<Double> prefireThrottle = SourceSystems.CONTROL.axis(Nums.CTRL, Nums.FIRE_LEVER);
+        Source<Double> triggerPrefire = prefireThrottle
+                .map(MapperSystems.CONTROL.deadZoneD(0.2D))
+//                .map(Data.mapper(d -> (1 - d) / 4D + 0.5D))
+                /*.inter(SourceSystems.CONTROL.button(Nums.CTRL_L, 1), Data.inter((v, t) -> t ? v : 0))*/;
+        Source<Double> triggerFire = SourceSystems.CONTROL.button(Nums.CTRL, Nums.TRIGGER)
                 .map(MapperSystems.CONTROL.buttonValue(0D, -65D));
         Source<Double> srcBallBelt = ballBeltCtrl.inter(triggerFire, Data.inter((a, b) -> Maths.clamp(a + b, -1D, 1D)));
 
         Sink<Double> ballBelt = SinkSystems.MOTOR.talonSrx(4).join(SinkSystems.MOTOR.talonSrx(6));
         Sink<Double> fireWheels = SinkSystems.MOTOR.talonSrx(3).join(SinkSystems.MOTOR.talonSrx(7).map(Funcs.invertD()));
 
+        Sink<Double> dashThrottle = SinkSystems.DASH.number("Throttle");
+        Sink<Double> dashShootMagn = SinkSystems.DASH.number("Cannon Magnitude");
+
         RobotMode.TELEOP.setOperation(() -> {
             drive.bind(srcDrive);
             ballBelt.bind(srcBallBelt);
             fireWheels.bind(triggerPrefire);
+//            dashThrottle.bind(throttle);
+            dashShootMagn.bind(prefireThrottle.map(Data.mapper(d -> (1 - d) / 2D)));
             Flow.waitInfinite();
         });
     }
@@ -68,7 +78,7 @@ public class Robot implements IRobotProgram {
                 stop = time + 100L;
                 return -0.5D;
             } else if (time < stop) {
-                return 0.2D;
+                return 0.25D;
             }
             return 0D;
         }
